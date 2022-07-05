e41c1dcfe63893b3e1149abfdfb1279c0c8ac2be
Algea Cao <algea.cao@rock-chips.com>
2022-07-05 17:48:30 +0800

    video/drm: dw-hdmi-qp: Support avi version 3
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>
    Change-Id: I79fdc8eb25f163f09ea2c72b4266f128b00cc331

--- drivers/video/drm/dw_hdmi_qp.c
+++ drivers/video/drm/dw_hdmi_qp.c
@@ -512,6 +512,19 @@ hdmi_get_tmdsclock(struct dw_hdmi_qp *hdmi, unsigned long mpixelclock)
 	return tmdsclock;
 }
 
+static void hdmi_infoframe_set_checksum(u8 *ptr, int size)
+{
+	u8 csum = 0;
+	int i;
+
+	ptr[3] = 0;
+	/* compute checksum */
+	for (i = 0; i < size; i++)
+		csum += ptr[i];
+
+	ptr[3] = 256 - csum;
+}
+
 static void hdmi_config_AVI(struct dw_hdmi_qp *hdmi, struct drm_display_mode *mode)
 {
 	struct hdmi_avi_infoframe frame;
@@ -571,6 +584,16 @@ static void hdmi_config_AVI(struct dw_hdmi_qp *hdmi, struct drm_display_mode *mo
 
 	frame.scan_mode = HDMI_SCAN_MODE_NONE;
 	frame.video_code = hdmi->vic;
+
+	/* mode which vic >= 128 must use avi version 3 */
+	if (hdmi->vic >= 128) {
+		frame.version = 3;
+		buff[1] = frame.version;
+		buff[4] &= 0x1f;
+		buff[4] |= ((frame.colorspace & 0x7) << 5);
+		buff[7] = frame.video_code;
+		hdmi_infoframe_set_checksum(buff, 17);
+	}
 
 	hdmi_avi_infoframe_pack_only(&frame, buff, 17);
 
